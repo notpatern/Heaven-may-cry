d9571e94f65ec4d4dcecb74e688fe93a286a0e0c
notpatern <132772266+notpatern@users.noreply.github.com>
2023-08-18 22:59:47 +0200

    Revert "yes"
    This reverts commit 6947f2981ebfc94a1e53ead0cf999348c9169f93.

--- Assets/Scripts/Movement/WallJump.cs
+++ Assets/Scripts/Movement/WallJump.cs
@@ -6,7 +6,6 @@ public class WallJump : MonoBehaviour
 {
     [Header("ref")]
     public Transform Player;
-    public LayerMask isWall;
 
     bool wallFront;
     bool wallLeft;
@@ -18,17 +17,10 @@ public class WallJump : MonoBehaviour
     {
         wallFront = Physics.Raycast(Player.orientation, Vector3.forward, wallDistance, isWall);
         wallBehind = Physics.Raycast(Player.orientation, -Vector3.forward, wallDistance, isWall);
-        wallLeft = Physics.Raycast(Player.orientation, -Vector3.right, wallDistance, isWall);
-        wallRight = Physics.Raycast(Player.orientation, Vector3.right, wallDistance, isWall);
-
-        if (wallFront) Debug.Log("Front");
-        if (wallBehind) Debug.Log("Back");
-        if (wallLeft) Debug.Log("Left");
-        if (wallRight) Debug.Log("Right");
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        Checks();
+        
     }
 }
